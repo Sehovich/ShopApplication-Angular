e4aa80296da44bec086613c07ba4f10b3e44b226
Armin Sehovic <armin.sehovic@qssbh.com>
2025-04-12 21:05:19 +0200

    Added Favorites and My Account pages with route protection and navbar links

---
+++ src/app/features/auth/features/auth/pages/my-account-page/my-account-page.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../../../../../shared/material.module';
+
+
+@Component({
+  standalone: true,
+  selector: 'app-my-account-page',
+  imports: [CommonModule, MaterialModule],
+  templateUrl: './my-account-page.component.html'
+})
+export class MyAccountPageComponent {
+  user = this.getUser();
+
+  getUser() {
+    const token = localStorage.getItem('token');
+    
+    return token ? { username: 'Logged In User', email: 'user@example.com' } : null;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    window.location.href = '/auth/login';
+  }
+}
