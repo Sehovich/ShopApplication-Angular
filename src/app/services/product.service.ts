3a9cc1042940c05893885778fd6b50298fbb3266
Armin Sehovic <armin.sehovic@qssbh.com>
2025-04-13 17:06:31 +0200

    Product service refactored for backend integration and caching

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,34 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product, ProductApiResponse } from '../models/product.model';
-import { Observable, of } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
-  private readonly apiUrl = 'https://dummyjson.com/products';
+
+  private readonly apiUrl = `${environment.apiUrl}/products`;
   private cachedPages = new Map<number, Product[]>(); 
 
   constructor(private http: HttpClient) {}
 
   getProductsByPage(page: number, limit: number): Observable<Product[]> {
-    const skip = (page - 1) * limit;
-
-    if (this.cachedPages.has(page)) {
-      return of(this.cachedPages.get(page)!);
-    }
-
-    return new Observable<Product[]>(observer => {
-      this.http
-        .get<ProductApiResponse>(`${this.apiUrl}?limit=${limit}&skip=${skip}`)
-        .subscribe({
-          next: (res) => {
-            this.cachedPages.set(page, res.products);
-            observer.next(res.products);
-            observer.complete();
-          },
-          error: (err) => observer.error(err)
-        });
-    });
+    const cached = this.cachedPages.get(page);
+    if (cached) return of(cached);
+  
+    return this.http.get<Product[]>(`${this.apiUrl}?page=${page}&pageSize=${limit}`).pipe(
+      tap(products => this.cachedPages.set(page, products))
+    );
   }
 
   clearCache() {
