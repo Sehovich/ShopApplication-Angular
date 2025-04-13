4fd0f295d5b60e27aba7bdc08472778bf757d2d1
Armin Sehovic <armin.sehovic@qssbh.com>
2025-04-13 12:20:30 +0200

    Updated routes and imports to match refactored containers folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,24 @@
 
 import { Routes } from '@angular/router';
-import { LoginPageComponent } from './features/auth/features/auth/pages/login-page/login-page.component';
-import { RegisterPageComponent } from './features/auth/features/auth/pages/register-page/register-page.component';
-import { ProductListPageComponent } from './features/auth/features/auth/pages/product-list-page/product-list-page.component';
-import { ProductDetailsPageComponent } from './features/auth/features/auth/pages/product-details-page/product-details-page.component';
-import { BasketPageComponent } from './features/auth/features/auth/pages/basket-page/basket-page.component';
+import { LoginPageComponent } from './features/auth/containers/login-page/login-page.component';
+import { RegisterPageComponent } from './features/auth/containers/register-page/register-page.component';
+import { ProductListPageComponent } from './features/auth/containers/product-list-page/product-list-page.component';
+import { ProductDetailsPageComponent } from './features/auth/containers/product-details-page/product-details-page.component';
+import { BasketPageComponent } from './features/auth/containers/basket-page/basket-page.component';
+import { FavoriteProductsPageComponent } from './features/auth/containers/favorite-products-page/favorite-products-page.component';
+import { MyAccountPageComponent } from './features/auth/containers/my-account-page/my-account-page.component';
 
 import { authGuard } from './core/guards/auth.guard';
-import { FavoriteProductsPageComponent } from './features/auth/features/auth/pages/favorite-products-page/favorite-products-page.component';
-import { MyAccountPageComponent } from './features/auth/features/auth/pages/my-account-page/my-account-page.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
-  { path: 'products', component: ProductListPageComponent },
-  { path: 'products/:id', component: ProductDetailsPageComponent },
-  { path: 'basket', component: BasketPageComponent, canActivate: [authGuard] },
-  { path: 'auth/login', component: LoginPageComponent },
-  { path: 'auth/register', component: RegisterPageComponent },
-  { path: 'favorites', component: FavoriteProductsPageComponent, canActivate: [authGuard] },
-  { path: 'account', component: MyAccountPageComponent, canActivate: [authGuard] },
-  { path: 'auth/login', component: LoginPageComponent },
-  { path: 'auth/register', component: RegisterPageComponent },
-];
+    { path: '', redirectTo: 'products', pathMatch: 'full' },
+    { path: 'products', component: ProductListPageComponent },
+    { path: 'products/:id', component: ProductDetailsPageComponent },
+    { path: 'basket', component: BasketPageComponent, canActivate: [authGuard] },
+    { path: 'auth/login', component: LoginPageComponent },
+    { path: 'auth/register', component: RegisterPageComponent },
+    { path: 'favorites', component: FavoriteProductsPageComponent, canActivate: [authGuard] },
+    { path: 'account', component: MyAccountPageComponent, canActivate: [authGuard] }
+  ];
+  
 
